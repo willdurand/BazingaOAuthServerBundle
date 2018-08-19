04800b5096d0347f95ed147258b449d3efd6d575
Nicole Cordes <typo3@cordes.co>
2018-08-19 19:00:08 +0200

    Correctly normalize parameters for oAuth authentication
    Currently oAuth authenfication for nested arrays is broken as those
    information is not decoded like the clients encode the information.
    Nested array handling is not covered at all.
    This patch simplifies the normalization handling and processes
    nested arrays in a proper way. Unfortunately oAuth documentation
    fails to give some expectations on how to normalize nested array
    information correctly, so each dimension is sorted on its own.

--- Service/OAuthAbstractServerService.php
+++ Service/OAuthAbstractServerService.php
@@ -293,34 +293,38 @@ abstract class OAuthAbstractServerService implements OAuthServerServiceInterface
 
     /**
      * Normalize request parameters.
+     *
      * @see http://oauth.net/core/1.O/#rfc.section.9.1.1
      *
-     * @param  array  $requestParameters An array of request parameters to normalize.
+     * @param  array $requestParameters An array of request parameters to normalize.
+     * @param string $parentKey
      * @return string
      */
-    protected function normalizeRequestParameters($requestParameters)
+    protected function normalizeRequestParameters($requestParameters, $parentKey = '')
     {
-        if (null === $requestParameters) {
+        if (empty($requestParameters)) {
             return '';
         }
 
-        ksort($requestParameters);
-
         $normalizedParameters = array();
         foreach ($requestParameters as $key => $value) {
-            if ('oauth_signature' !== $key) {
-                if (is_array($value)) {
-                    $sortedValues = $value;
+            if ('oauth_signature' === $key) {
+                continue;
+            }
 
-                    sort($sortedValues);
-                    foreach ($sortedValues as $sortedValue) {
-                        $normalizedParameters[] = rawurlencode($key) . '=' . rawurlencode($sortedValue);
-                    }
-                } else {
-                    $normalizedParameters[] = rawurlencode($key) . '=' . rawurlencode($value);
-                }
+            if (!empty($parentKey)) {
+                // Multidimensional array; using foo=bar&foo=baz rather than foo[bar]=baz&foo[baz]=bar
+                $key = $parentKey;
+            }
+
+            if (is_array($value)) {
+                $normalizedParameters[] = $this->normalizeRequestParameters($value, $key);
+            } else {
+                $normalizedParameters[] = rawurlencode($key) . '=' . rawurlencode($value);
             }
         }
+
+        sort($normalizedParameters, SORT_STRING);
 
         return implode('&', $normalizedParameters);
     }
